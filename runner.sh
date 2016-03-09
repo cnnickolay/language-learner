1e05b4e0a91945ddb06fcf7cb834109f94a0dca9
root <root@alpha.net>
2016-03-09 16:27:40 +0300

    script removed

--- runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-bower --allow-root install
-./activator dist
-cd target/universal && rm -rf language-learner-1.0-SNAPSHOT && unzip language-learner-1.0-SNAPSHOT.zip && cd language-learner-1.0-SNAPSHOT
-
-nohup ./bin/language-learner -Dconfig.file=/tmp/language-learner/conf/application_prod.conf -Dplay.evolutions.db.default.autoApply=true -Dhttp.port=80 &> /var/log/language-learner/language-learner.log &
